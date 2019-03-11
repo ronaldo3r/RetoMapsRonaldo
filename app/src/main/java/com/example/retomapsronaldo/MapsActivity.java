c372135f81eae1c6aed39cd4eaabbcd8b3ca125a
ronaldo3r <ronaldo3-0827@hotmail.com>
2019-03-11 13:57:24 -0500

    casi final

--- app/src/main/java/com/example/retomapsronaldo/MapsActivity.java
+++ app/src/main/java/com/example/retomapsronaldo/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.retomapsronaldo;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,20 +16,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private static final int REQUEST_CODE = 11 ;
+    private static final int REQUEST_CODE = 11;
     private GoogleMap mapa;
     private LocationManager manager;
     private ArrayList<Marker> marcadores;
@@ -61,7 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         marcadores = new ArrayList<Marker>();
 
         //Dialogo
-        dialog_agregar= new Dialog(this);
+        dialog_agregar = new Dialog(this);
         dialog_agregar.setContentView(R.layout.dialogo_agregar);
         txt_dialog = dialog_agregar.findViewById(R.id.txt_dialog);
         edit_nombre = dialog_agregar.findViewById(R.id.edit_name);
@@ -93,19 +98,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Manifest.permission.ACCESS_COARSE_LOCATION
         }, REQUEST_CODE);
 
-
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(4, -72);
-//        mapa.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mapa.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
         //Agregar el listener de ubicacion
         manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                Log.e(">>>","LAT: "+location.getLatitude()+ " , LONG: "+location.getLongitude());
+                Log.e(">>>", "LAT: " + location.getLatitude() + " , LONG: " + location.getLongitude());
 
-                if(inicio != null){
+                if (inicio != null) {
                     inicio.remove();
                 }
                 inicio = mapa.addMarker(new MarkerOptions()
@@ -114,7 +113,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 );
                 //inicio.setIcon();
                 mapa.moveCamera(CameraUpdateFactory
-                        .newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 10));
+                        .newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15));
+
+
+                if (!marcadores.isEmpty()) {
+                    String mens = estaCerca();
+                    lugarCercano.setText(mens);
+                }
+
             }
 
             @Override
@@ -138,10 +144,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onMapClick(LatLng latLng) {
 
-                latLongitud=latLng;
+                latLongitud = latLng;
                 agregarMarcador();
-//                masCercano=calcularMasCercano();
-//                lugarCercano.setText("El lugar más cercano es: "+masCercano.getTitle());
 
             }
         });
@@ -149,13 +153,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapa.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
+
+                if (marker.equals(inicio)) {
+
+                    try {
+                        Geocoder geo = new Geocoder(MapsActivity.this.getApplicationContext(), Locale.getDefault());
+                        List<Address> addresses = geo.getFromLocation(inicio.getPosition().latitude, marker.getPosition().longitude, 1);
+                        if (addresses.isEmpty()) {
+                            Toast.makeText(getApplicationContext(), "Esperando por la dirección...", Toast.LENGTH_LONG).show();
+                        } else {
+                            if (addresses.size() > 0) {
+                                marker.setSnippet("Usted se encuentra en " + addresses.get(0).getAddressLine(0) + addresses.get(0).getAdminArea());
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace(); // getFromLocation() may sometimes fail
+                    }
+
+
+                } else {
+
+                    int distancia = calcularDistanciaMarcadores(marker);
+
+                    marker.setSnippet("Para llegar a este lugar faltan " + distancia + " metros");
+                }
                 return false;
             }
         });
 
     }
 
-    public void agregarMarcador(){
+    //Método para agregar marcador
+    public void agregarMarcador() {
 
         btn_cancelar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -167,51 +196,95 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         btn_agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Marker actual = mapa.addMarker(new MarkerOptions()
-                        .position(latLongitud)
-                        .title(edit_nombre.getText().toString()));
-                marcadores.add(actual);
-                masCercano=calcularMasCercano();
-                lugarCercano.setText("El lugar más cercano es: "+masCercano.getTitle());
-                edit_nombre.setText("");
-                dialog_agregar.dismiss();
+                if ((edit_nombre.getText().toString() != null) && !(edit_nombre.getText().toString().equals(""))&&!(edit_nombre.getText().toString().trim().equals(""))) {
+                    Marker actual = mapa.addMarker(new MarkerOptions()
+                            .position(latLongitud)
+                            .title(edit_nombre.getText().toString()));
+                    marcadores.add(actual);
+                    String mens = estaCerca();
+                    lugarCercano.setText(mens);
+                    edit_nombre.setText("");
+                    dialog_agregar.dismiss();
+                }
             }
         });
 
         dialog_agregar.show();
     }
 
-    public Marker calcularMasCercano(){
+    //Calcular marcador mas cercano a tu posición actual
+    public Marker calcularMasCercano() {
 
         Marker marMenor = null;
-        double disMenor=0;
-        Location miPos= new Location("Mi posición");
+        double disMenor = 0;
+        Location miPos = new Location("Mi posición");
         miPos.setLatitude(inicio.getPosition().latitude);
         miPos.setLongitude(inicio.getPosition().longitude);
 
 
-        for(int i=0; i<marcadores.size();i++){
+        for (int i = 0; i < marcadores.size(); i++) {
 
-            Marker temporal=marcadores.get(i);
+            Marker temporal = marcadores.get(i);
 
-            Location pos= new Location("Otra posición");
+            Location pos = new Location("Otra posición");
             pos.setLatitude(temporal.getPosition().latitude);
             pos.setLongitude(temporal.getPosition().longitude);
 
-            double disTemporal=miPos.distanceTo(pos);
+            double disTemporal = miPos.distanceTo(pos);
 
-            if(i==0) {
+            if (i == 0) {
                 disMenor = miPos.distanceTo(pos);
-                marMenor=temporal;
+                marMenor = temporal;
             }
 
-            if(disMenor>disTemporal){
-                disMenor=disTemporal;
-                marMenor=temporal;
+            if (disMenor > disTemporal) {
+                disMenor = disTemporal;
+                marMenor = temporal;
             }
 
         }
 
         return marMenor;
+    }
+
+    //Calcular distancia entre posición actual y otro marcador
+    public int calcularDistanciaMarcadores(Marker uno) {
+
+        double distancia = 0;
+        int dist = 0;
+
+        Location miPos = new Location("Otra posición");
+        miPos.setLatitude(inicio.getPosition().latitude);
+        miPos.setLongitude(inicio.getPosition().longitude);
+
+        Location pos = new Location("Otra posición");
+        pos.setLatitude(uno.getPosition().latitude);
+        pos.setLongitude(uno.getPosition().longitude);
+
+        distancia = miPos.distanceTo(pos);
+        dist = (int) distancia;
+
+        return dist;
+    }
+
+    public String estaCerca() {
+
+        String mensaje = "";
+
+        masCercano = calcularMasCercano();
+
+        int distancia = calcularDistanciaMarcadores(masCercano);
+
+        if (distancia <= 500) {
+
+            mensaje = "Para llegar a " + masCercano.getTitle() + " faltan " + distancia + " metros";
+        } else {
+
+            mensaje = "El lugar más cercano es:" + masCercano.getTitle();
+        }
+
+
+        return mensaje;
+
     }
 }
